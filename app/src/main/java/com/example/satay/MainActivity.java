1fbeb9f4955d877ccdf70a905bc5348aaafd9edb
Udin said <nashtv61@gmail.com>
2021-10-14 16:54:07 +0800

    Static Login(Tested) 20211014

--- app/src/main/java/com/example/satay/MainActivity.java
+++ app/src/main/java/com/example/satay/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.satay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username= (TextView)findViewById(R.id.Username);
+        TextView password=(TextView) findViewById(R.id.passwordL);
+
+        MaterialButton loginbtn= (MaterialButton)findViewById(R.id.btnlogin);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
+                    Toast.makeText(MainActivity.this, "Login Sucessful", Toast.LENGTH_SHORT).show();
+
+
+        }
+        });
     }
 }
